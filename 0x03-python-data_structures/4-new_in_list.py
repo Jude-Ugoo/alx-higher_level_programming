5cec60b5967ad404d80c59d1a512a9e67ec976d6
Jude-Ugoo <ugooeze0@gmail.com>
2023-12-04 06:11:16 -0800

    fixed bugs

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/pythoni3
 
 def new_in_list(my_list, idx, element):
     dup_list = my_list.copy()
@@ -6,7 +6,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    for i in dup_list:
-        if i == idx:
+    while idx < len(my_list):
             dup_list[idx] = element
             return dup_list
